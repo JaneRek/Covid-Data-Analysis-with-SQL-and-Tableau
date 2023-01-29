6b9dc00bf540faad3d233cab0c7a2bae5822f8d2
Jane <80624347+JaneRek@users.noreply.github.com>
2023-01-29 14:12:08 +0100

    Add files via upload

---
+++ QueriesForTableau.sql
@@ -0,0 +1,40 @@
+/*
+Queries used for Tableau Visualization
+*/
+
+
+-- 1. Get the total number of cases, deaths, death percentage for all years
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. Total deaths from Covid per Continents
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Total percent of population infected with Covid Per Country by January 2023
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Time span of percent of population infected with Covid Per Country 
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
